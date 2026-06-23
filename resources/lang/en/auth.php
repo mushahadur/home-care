b32d4008ac89c3156d0046b3a28a6d40aae106c5
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2026-06-23 23:17:34 +0600

    initial set language home page and auth page recommit

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return[
+    
+    'login' => 'Login',
+    'register' => 'Register',
+    'name' => 'Enter Full Name',
+    'email' => 'Enter Email Address ',
+    'password' => 'Enter Password',
+    'confirm_password' => 'Enter Confirm Password',
+    'agree' => 'I agree to the Terms and privacy policy.',
+    'remember' => 'Remember me',
+    'forgot_password' => 'Forgot password ?',
+    'login_btn' => 'Sign in',
+    'register_btn' => 'Create Account',
+    'have_not' => 'Don’t have an account?',
+    'have' => 'Already have an account? ',
+
+ ];
---
+++ resources/lang/en/hero.php
@@ -0,0 +1,12 @@
+<?php
+return[
+    
+    'subtitle' => 'Only one trusted Home Care',
+    'title_1' => 'Reputable',
+    'title_2' => 'Home Care',
+    'description_1' => 'Professional nursing care in the comfort of your own home — ',
+    'description_2' => 'a trusted institution in Brahmanbaria.',
+    'contact_btn' => 'Contact Us',
+    'service_btn' => 'Our Services',
+
+ ];
